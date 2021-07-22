5c680f104b493162db4d73092105abc5d043e371
zahar240 <73786090+zahar240@users.noreply.github.com>
2021-07-23 01:12:25 +0300

    add tk_isogramdetektor

---
+++ tk_isogramdetektor.py
@@ -0,0 +1,39 @@
+# 1. Task
+"""
+An isogram is a word that has no repeating letters, whether they are consecutive or non-consecutive.
+Изограмма-это слово, в котором нет повторяющихся букв, независимо от того, являются ли они последовательными или не последовательными.  
+Your job is to find a way to detect if a word is an isogram.
+Ваша задача-найти способ определить, является ли слово изограммой.
+
+Task: Write a program that takes in a string as input, detects if the string is an isogram and outputs true or false based on the result.
+Задача: Напишите программу, которая принимает строку в качестве входных данных, определяет, является ли строка изограммой, /n 
+и выводит значение true или false на основе результата.
+
+Input Format:
+Формат Ввода: 
+A string containing one word.
+Строка, содержащая одно слово.
+
+Output Format:
+Формат Вывода: 
+A string: true or false.
+Строка: истина или ложь.
+
+Sample Input:
+Ввод Образца: 
+turbulence
+турбулентность
+"""
+
+
+# 2. Solution logic
+"""
+1. Прописать переменную, которая получает значение пользовательским вводом, тип данных - строка.
+2. Прописать список(или строку) содердащую все элементы алфавита: a-z.
+3. Прописать логику, при которой элементы строки, полученной  в п.1 поиндексно проверяются на наличие в строке полученной в п.2 /n 
+   подсчитываются, сравниваются.
+4. Вывести полученный результат.
+"""
+
+
+# 3. Solution
