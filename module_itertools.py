e6e954b1eb9763aebf3b08035d3c699c29eff1c0
zahar240 <73786090+zahar240@users.noreply.github.com>
2021-08-15 13:42:46 +0300

    add_fix_module_itertools

--- module_itertools.py
+++ module_itertools.py
@@ -1,7 +1,11 @@
 # Модуль itertools
 
+# count - создает бесконечную прогессию вверх от заданного числа
 from itertools import count
+# accumulate - возвращает сумму значений внутри итерируемого объекта
+# takewhile - возвращает элементы из итерируемого объекта, которые удовлетворяют предикативной функции
 from itertools import accumulate, takewhile
+# product, permutations - используются, когда нужно выполнить задачу со всеми возможными коминациями некоторых элементов
 from itertools import product, permutations
 
 for i in count(3):
