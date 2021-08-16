4d7b5181bf290dc11b4dc9a94085df98f2cb48b2
zahar240 <73786090+zahar240@users.noreply.github.com>
2021-08-17 01:22:20 +0300

    add_class

--- class.py
+++ class.py
@@ -9,3 +9,28 @@ stumpy = Cat("brown", 3)
 
 print(felix.color)
 print(felix.legs)
+
+
+class Dog:
+    legs = 4
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+
+    def bark(self):
+        print("Woof!")
+
+fido = Dog("Fido", "brown")
+print(fido.name)
+fido.bark()
+
+print(fido.legs)
+print(Dog.legs)
+
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+rect = Rectangle(7, 8)
+print(rect.color)
