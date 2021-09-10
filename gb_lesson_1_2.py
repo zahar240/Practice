abd2d7125cfd362dca45db05c8240de88b058ed8
Alexey <zakharchenko_240@mail.ru>
2021-09-11 01:01:08 +0300

    add change

--- gb_lesson_1_2.py
+++ gb_lesson_1_2.py
@@ -4,16 +4,28 @@ a) Вычислить сумму тех чисел из этого списка,
 Внимание: использовать только арифметические операции!
 b) К каждому элементу списка добавить 17 и заново вычислить сумму тех чисел из этого списка, сумма цифр которых делится нацело на 7. 
 c) * Решить задачу под пунктом b, не создавая новый список.
+
+17485588610
+15392909930
 """
 
 list_cube = []
-sum_number_cube = 0
+list_cube_multiple_7 = []
+sum_number_cube_multiple_7 = 0
+
 for number in range(1, 101):
     if number %2 == 1:
         list_cube.append(number ** 3)
-        for number_cube in list_cube:
-            if (number_cube // 10 ** 6 + number_cube // 10 ** 5 % 10 + number_cube // 10 ** 4 % 10 + number_cube // 10 ** 3 % 10 + number_cube // 10 ** 2 % 10 +number_cube // 10 % 10 + number_cube % 10) %7 == 0:
-                sum_number_cube += number_cube
-        print(sum_number_cube)
+
+for number_cube in list_cube:
+    sum_sing_number = 0
+    while number_cube > 0:
+        sum_sing_number = sum_sing_number + (number_cube %10)
+        number_cube = number_cube // 10
+    
+   
+
 
 print(list_cube)
+print(list_cube_multiple_7)
+print(sum_number_cube_multiple_7)
