e3ef9f21d1b37bf783e5ea8b891ad9e00a6c3a6d
Alexey <zakharchenko_240@mail.ru>
2021-09-21 22:18:57 +0300

    add gb_task_3_4

--- gb_lesson_3_4.py
+++ gb_lesson_3_4.py
@@ -15,3 +15,31 @@
     }
 }
 Как поступить, если потребуется сортировка по ключам?"""
+
+
+from os import name
+
+
+def thesaurus_adv(*names):
+    names_dict = {}
+    for name in sorted(names):
+        n, s = name.split()
+        val = names_dict.setdefault(s[0], {n[0]: name})
+        n_val = val.setdefault(n[0], [name])
+        if name not in n_val:
+            n_val.append(name)
+    return names_dict
+
+
+def thesaurus_adv_2(*names):
+    s_n_sort = {}
+    for s_n in names:
+        s_n.sort.setdefault(s_n.split()[1][0], {}).setdefault(s_n.split[0][0], []).append(s_n)
+    return s_n_sort
+
+
+list_names = ("Qqqq Wwwww", "Aaaaa Ssssss", "Zzzz Xxxxxxx", "Eeeee Rrrrrr", "Tttt Uuuuuu",
+"Cccc Bbbbb", "Nnnn Mmmmm", "Hhhh Kkkkkk", "Iiii Ooooo", "Pppp Gggggg")
+
+print(thesaurus_adv(list_names))
+print(thesaurus_adv_2(list_names))
